7086162dcd698932a848f60272e63ad4e9942400
Kingsley Chijioke <kumaseyin4@gmail.com>
2021-03-05 14:13:04 +0100

    RESTful API and Testing Project

---
+++ routes/api_routes.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+const contactController = require('../controllers/contactController')
+
+router.get('/', (req, res) => {
+    res.json({
+        status: 'API is working',
+        message: 'Welcome to restPractice!'
+    });
+});
+
+router.route('/contacts')
+    .get(contactController.index)
+    .post(contactController.new);
+
+router.route('/contacts/:contact_id')
+    .get(contactController.view)
+    .patch(contactController.update)
+    .put(contactController.update)
+    .delete(contactController.delete);
+
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const apiRoutes = require('./routes/api_routes');
+const bodyPaser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+
+const PORT = process.env.PORT || 3500;
+
+app.get('/', (req, res) => res.send('Hello world with Express'));
+
+app.listen(PORT, () => {
+    console.log('Running restPractice on port: ' + PORT)
+});
+
+app.use('/api', apiRoutes);
+
+app.use(bodyPaser.urlencoded({
+    extended: true
+}));
+
+app.use(bodyPaser.json());
+
+mongoose.connect('mongodb://localhost:27017/restPractice', { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+
+if(!db){
+    console.log('Error connecting to DB')
+}else{
+    console.log('DB connected Successfully')
+}
